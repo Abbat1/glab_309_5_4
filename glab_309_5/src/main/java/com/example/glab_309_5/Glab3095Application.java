1d214e3b02d432e55c23e7a0f3ea8f33ff67807a
Abbat1 <cozzyyist@gmail.com>
2024-09-17 09:45:01 -0500

    glab_309_5_4

---
+++ glab_309_5/src/main/java/com/example/glab_309_5/Glab3095Application.java
@@ -0,0 +1,13 @@
+package com.example.glab_309_5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Glab3095Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Glab3095Application.class, args);
+	}
+
+}
